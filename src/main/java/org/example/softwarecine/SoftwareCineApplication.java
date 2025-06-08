c6d4de66f9ac2447766884ea38786a7519d7b691
Fabris05 <fabricioslazar0307@gmail.com>
2025-06-08 03:12:58 -0500

    first commit

---
+++ src/main/java/org/example/softwarecine/SoftwareCineApplication.java
@@ -0,0 +1,13 @@
+package org.example.softwarecine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareCineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareCineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/softwarecine/SoftwareCineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.softwarecine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareCineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
